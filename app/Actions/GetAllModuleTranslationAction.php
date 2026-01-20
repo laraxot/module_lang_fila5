11c3cbfe165dbfb707233d198bb824c42aa619a4
marco76tv <marco.sottana@gmail.com>
2026-01-20 21:04:41 +0100

    .

--- app/Actions/GetAllModuleTranslationAction.php
+++ app/Actions/GetAllModuleTranslationAction.php
@@ -7,7 +7,7 @@ namespace Modules\Lang\Actions;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
-use function Safe\glob;
+// use function Safe\glob;
 
 use Spatie\QueueableAction\QueueableAction;
 
--- app/Filament/Resources/Pages/LangBaseEditRecord.php
+++ app/Filament/Resources/Pages/LangBaseEditRecord.php
@@ -4,20 +4,20 @@ declare(strict_types=1);
 
 namespace Modules\Lang\Filament\Resources\Pages;
 
-use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
-use LaraZeus\SpatieTranslatable\Resources\Pages\EditRecord\Concerns\Translatable;
+// use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
+// use LaraZeus\SpatieTranslatable\Resources\Pages\EditRecord\Concerns\Translatable;
 use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
 abstract class LangBaseEditRecord extends XotBaseEditRecord
 {
-    use Translatable;
+    // use Translatable; // Temporarily disabled until lara-zeus package is working
 
     protected static string $resource; // = SectionResource::class;
 
     protected function getHeaderActions(): array
     {
         return array_merge(
-            ['locale-switcher' => LocaleSwitcher::make()],
+            // ['locale-switcher' => LocaleSwitcher::make()], // Temporarily disabled until lara-zeus package is working
             parent::getHeaderActions(),
         );
     }
