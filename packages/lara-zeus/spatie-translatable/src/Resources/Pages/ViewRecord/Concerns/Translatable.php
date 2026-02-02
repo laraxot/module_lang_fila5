65a38eb09068e4c4d9d17f4aed696d3d6b314f3e
marco76tv <marco76tv@users.noreply.github.com>
2026-02-02 10:45:44 +0000

    Check & fix styling

--- packages/lara-zeus/spatie-translatable/src/Resources/Pages/ViewRecord/Concerns/Translatable.php
+++ packages/lara-zeus/spatie-translatable/src/Resources/Pages/ViewRecord/Concerns/Translatable.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Arr;
 use LaraZeus\SpatieTranslatable\Resources\Concerns\HasActiveLocaleSwitcher;
 use LaraZeus\SpatieTranslatable\Resources\Pages\Concerns\HasTranslatableFormWithExistingRecordData;
 use LaraZeus\SpatieTranslatable\Resources\Pages\Concerns\HasTranslatableRecord;
-use RuntimeException;
-use Throwable;
 
 trait Translatable
 {
@@ -19,13 +17,13 @@ trait Translatable
     use HasTranslatableRecord;
 
     /**
-     * @throws Throwable
+     * @throws \Throwable
      */
     public function bootTranslatable(): void
     {
         throw_unless(
             is_subclass_of(static::class, ViewRecord::class),
-            new RuntimeException('dont use the trait "' . Translatable::class . '" with "' . static::class . '"')
+            new \RuntimeException('dont use the trait "'.Translatable::class.'" with "'.static::class.'"')
         );
 
         $this->activeLocale = $this->getStoredActiveLocale()
