846c5c4a2e628936a4d8da58c67a8b6258f62d50
marco76tv <marco.sottana@gmail.com>
2026-02-09 15:21:51 +0100

    .

---
+++ lang/en/header.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'cta' => 'Request Consultation',
+    'dashboard' => 'Dashboard',
+    'profile' => 'Profile',
+    'settings' => 'Settings',
+    'logout' => 'Logout',
+    'login' => 'Login',
+    'language' => 'Language',
+];
