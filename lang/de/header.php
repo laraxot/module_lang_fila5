636fed922acc1973ad2276dda95e2310b8df3c40
marco76tv <marco.sottana@gmail.com>
2026-02-09 15:21:51 +0100

    .

---
+++ lang/de/header.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'cta' => 'Konsultation anfordern',
+    'dashboard' => 'Dashboard',
+    'profile' => 'Profil',
+    'settings' => 'Einstellungen',
+    'logout' => 'Abmelden',
+    'login' => 'Anmelden',
+    'language' => 'Sprache',
+];
