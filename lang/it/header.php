5391ba7169e11a090ab759aacf9a702d1a571d62
marco76tv <marco.sottana@gmail.com>
2026-02-10 13:53:32 +0100

    .

---
+++ lang/it/header.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'cta' => 'Richiedi Consulenza',
+    'dashboard' => 'Dashboard',
+    'profile' => 'Profilo',
+    'settings' => 'Impostazioni',
+    'logout' => 'Esci',
+    'login' => 'Accedi',
+    'language' => 'Lingua',
+];
