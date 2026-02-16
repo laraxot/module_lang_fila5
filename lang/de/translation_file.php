f507a1ae7977d6568536f40433a6134d7999182f
marco76tv <marco76tv@users.noreply.github.com>
2026-02-16 21:19:54 +0000

    Check & fix styling

--- lang/de/translation_file.php
+++ lang/de/translation_file.php
@@ -2,115 +2,96 @@
 
 declare(strict_types=1);
 
-return array (
-  'actions' => 
-  array (
-    'create' => 
-    array (
-      'label' => 'create',
-    ),
-    'lang' => 
-    array (
-      'label' => 'lang',
-    ),
-  ),
-  'fields' => 
-  array (
-    'edit' => 
-    array (
-      'label' => 'edit',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'toggleColumns' => 
-    array (
-      'label' => 'toggleColumns',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'reorderRecords' => 
-    array (
-      'label' => 'reorderRecords',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'resetFilters' => 
-    array (
-      'label' => 'resetFilters',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'content' => 
-    array (
-      'description' => 'content',
-      'helper_text' => 'content',
-      'placeholder' => 'content',
-      'label' => '',
-      'tooltip' => '',
-    ),
-    'applyFilters' => 
-    array (
-      'label' => 'applyFilters',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'snapshots' => 
-    array (
-      'fields' => 
-      array (
-        'updated_at' => 
-        array (
-          'help' => 
-          array (
-            'description' => 'snapshots.fields.updated_at.help',
-            'helper_text' => 'snapshots.fields.updated_at.help',
-            'placeholder' => 'snapshots.fields.updated_at.help',
-            'label' => 'snapshots.fields.updated_at.help',
-          ),
-          'label' => 
-          array (
-            'description' => 'snapshots.fields.updated_at.label',
-            'helper_text' => 'snapshots.fields.updated_at.label',
-            'placeholder' => 'snapshots.fields.updated_at.label',
-          ),
-        ),
-      ),
-      'label' => '',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'openFilters' => 
-    array (
-      'label' => 'openFilters',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'key' => 
-    array (
-      'label' => 'key',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-  ),
-  'navigation' => 
-  array (
-    'label' => 'Navigation Label',
-    'sort' => '73',
-    'icon' => 'heroicon-o-cog',
-    'group' => 'Lang',
-  ),
-  'model' => 
-  array (
-    'label' => 'translation file.model',
-  ),
-  'label' => 'Missing Label',
-  'plural_label' => 'Missing Plural label',
-);
+return [
+    'actions' => [
+        'create' => [
+            'label' => 'create',
+        ],
+        'lang' => [
+            'label' => 'lang',
+        ],
+    ],
+    'fields' => [
+        'edit' => [
+            'label' => 'edit',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'toggleColumns' => [
+            'label' => 'toggleColumns',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'reorderRecords' => [
+            'label' => 'reorderRecords',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'resetFilters' => [
+            'label' => 'resetFilters',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'content' => [
+            'description' => 'content',
+            'helper_text' => 'content',
+            'placeholder' => 'content',
+            'label' => '',
+            'tooltip' => '',
+        ],
+        'applyFilters' => [
+            'label' => 'applyFilters',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'snapshots' => [
+            'fields' => [
+                'updated_at' => [
+                    'help' => [
+                        'description' => 'snapshots.fields.updated_at.help',
+                        'helper_text' => 'snapshots.fields.updated_at.help',
+                        'placeholder' => 'snapshots.fields.updated_at.help',
+                        'label' => 'snapshots.fields.updated_at.help',
+                    ],
+                    'label' => [
+                        'description' => 'snapshots.fields.updated_at.label',
+                        'helper_text' => 'snapshots.fields.updated_at.label',
+                        'placeholder' => 'snapshots.fields.updated_at.label',
+                    ],
+                ],
+            ],
+            'label' => '',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'openFilters' => [
+            'label' => 'openFilters',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'key' => [
+            'label' => 'key',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+    ],
+    'navigation' => [
+        'label' => 'Navigation Label',
+        'sort' => '73',
+        'icon' => 'heroicon-o-cog',
+        'group' => 'Lang',
+    ],
+    'model' => [
+        'label' => 'translation file.model',
+    ],
+    'label' => 'Missing Label',
+    'plural_label' => 'Missing Plural label',
+];
--- resources/lang/it/translation.php
+++ resources/lang/it/translation.php
@@ -2,201 +2,168 @@
 
 declare(strict_types=1);
 
-return array (
-  'navigation' => 
-  array (
-    'name' => 'Traduzione',
-    'plural' => 'Traduzioni',
-    'group' => 
-    array (
-      'name' => 'Admin',
-    ),
-  ),
-  'pages' => 
-  array (
-    'create' => 'Nuovo Tecnico',
-    'edit' => 'Modifica Tecnico',
-    'view' => 'Tecnico',
-    'list_technicians' => 
-    array (
-      'navigation' => 
-      array (
-        'name' => 'Tecnici',
-        'plural' => 'Tecnici',
-        'group' => 
-        array (
-          'name' => 'Gestione Utenti',
-        ),
-      ),
-      'fields' => 
-      array (
-        'user_name' => 'Nome Utente',
-        'name' => 'Nome Utente',
-        'first_name' => 'Nome',
-        'last_name' => 'Cognome',
-        'email' => 'Email',
-        'is_active' => 'Stato account',
-        'color' => 'Colore',
-        'asset_id_root' => 'Abitazione',
-        'asset_id' => 'Asset',
-        'type' => 'Tipo',
-      ),
-    ),
-  ),
-  'fields' => 
-  array (
-    'id' => 
-    array (
-      'label' => 'ID',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'lang' => 
-    array (
-      'label' => 'Lingua',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'value' => 
-    array (
-      'label' => 'Valore',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'key' => 
-    array (
-      'label' => 'Chiave',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'namespace' => 
-    array (
-      'label' => 'Namespace',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'group' => 
-    array (
-      'label' => 'Gruppo',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'item' => 
-    array (
-      'label' => 'Elemento',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'name' => 
-    array (
-      'label' => 'Nome Utente',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'first_name' => 
-    array (
-      'label' => 'Nome',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'last_name' => 
-    array (
-      'label' => 'Cognome',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'email' => 
-    array (
-      'label' => 'Email',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'is_active' => 
-    array (
-      'label' => 'Stato account',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'color' => 
-    array (
-      'label' => 'Colore',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'asset_id_root' => 
-    array (
-      'label' => 'Abitazione',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'asset_id' => 
-    array (
-      'label' => 'Asset',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'type' => 
-    array (
-      'label' => 'tipo',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-    'user_name' => 
-    array (
-      'label' => 'nome utente',
-      'tooltip' => '',
-      'helper_text' => '',
-      'description' => '',
-    ),
-  ),
-  'filters' => 
-  array (
-    'is_active' => 
-    array (
-      'all' => 'Tutti i tecnici',
-      'active' => 'Solo attivi',
-      'inactive' => 'Solo inattivi',
-    ),
-  ),
-  'actions' => 
-  array (
-    'bulk_activate' => 
-    array (
-      'cta' => 'Attiva selezionati',
-    ),
-    'bulk_inactivate' => 
-    array (
-      'cta' => 'Disattiva selezionati',
-    ),
-    'is_active_on' => 
-    array (
-      'cta' => 'Abilita account',
-    ),
-    'is_active_off' => 
-    array (
-      'cta' => 'Disabilita account',
-    ),
-  ),
-  'act' => 
-  array (
-    'publish_item_trans' => 'pubblica modifiche riga',
-  ),
-  'label' => 'Missing Label',
-  'plural_label' => 'Missing Plural label',
-);
+return [
+    'navigation' => [
+        'name' => 'Traduzione',
+        'plural' => 'Traduzioni',
+        'group' => [
+            'name' => 'Admin',
+        ],
+    ],
+    'pages' => [
+        'create' => 'Nuovo Tecnico',
+        'edit' => 'Modifica Tecnico',
+        'view' => 'Tecnico',
+        'list_technicians' => [
+            'navigation' => [
+                'name' => 'Tecnici',
+                'plural' => 'Tecnici',
+                'group' => [
+                    'name' => 'Gestione Utenti',
+                ],
+            ],
+            'fields' => [
+                'user_name' => 'Nome Utente',
+                'name' => 'Nome Utente',
+                'first_name' => 'Nome',
+                'last_name' => 'Cognome',
+                'email' => 'Email',
+                'is_active' => 'Stato account',
+                'color' => 'Colore',
+                'asset_id_root' => 'Abitazione',
+                'asset_id' => 'Asset',
+                'type' => 'Tipo',
+            ],
+        ],
+    ],
+    'fields' => [
+        'id' => [
+            'label' => 'ID',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'lang' => [
+            'label' => 'Lingua',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'value' => [
+            'label' => 'Valore',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'key' => [
+            'label' => 'Chiave',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'namespace' => [
+            'label' => 'Namespace',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'group' => [
+            'label' => 'Gruppo',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'item' => [
+            'label' => 'Elemento',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'name' => [
+            'label' => 'Nome Utente',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'first_name' => [
+            'label' => 'Nome',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'last_name' => [
+            'label' => 'Cognome',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'email' => [
+            'label' => 'Email',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'is_active' => [
+            'label' => 'Stato account',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'color' => [
+            'label' => 'Colore',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'asset_id_root' => [
+            'label' => 'Abitazione',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'asset_id' => [
+            'label' => 'Asset',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'type' => [
+            'label' => 'tipo',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'user_name' => [
+            'label' => 'nome utente',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+    ],
+    'filters' => [
+        'is_active' => [
+            'all' => 'Tutti i tecnici',
+            'active' => 'Solo attivi',
+            'inactive' => 'Solo inattivi',
+        ],
+    ],
+    'actions' => [
+        'bulk_activate' => [
+            'cta' => 'Attiva selezionati',
+        ],
+        'bulk_inactivate' => [
+            'cta' => 'Disattiva selezionati',
+        ],
+        'is_active_on' => [
+            'cta' => 'Abilita account',
+        ],
+        'is_active_off' => [
+            'cta' => 'Disabilita account',
+        ],
+    ],
+    'act' => [
+        'publish_item_trans' => 'pubblica modifiche riga',
+    ],
+    'label' => 'Missing Label',
+    'plural_label' => 'Missing Plural label',
+];
