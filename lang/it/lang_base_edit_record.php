7e34964f65b8ac65aa78bff716c46d8d028add54
marco76tv <marco.sottana@gmail.com>
2026-02-16 10:50:58 +0100

    .

--- lang/it/lang_base_edit_record.php
+++ lang/it/lang_base_edit_record.php
@@ -8,4 +8,29 @@ return [
             'label' => 'activeLocale',
         ],
     ],
+    'label' => 'Lang Base Edit Record',
+    'plural_label' => 'Lang Base Edit Record (Plurale)',
+    'navigation' => [
+        'name' => 'Lang Base Edit Record',
+        'plural' => 'Lang Base Edit Record',
+        'group' => [
+            'name' => 'General',
+            'description' => 'General Settings',
+        ],
+        'label' => 'Lang Base Edit Record',
+        'sort' => 1,
+        'icon' => 'heroicon-o-collection',
+    ],
+    'fields' => [
+        'id' => [
+            'label' => 'Identificativo',
+            'tooltip' => 'Identificativo univoco del record',
+        ],
+        'created_at' => [
+            'label' => 'Data Creazione',
+        ],
+        'updated_at' => [
+            'label' => 'Ultima Modifica',
+        ],
+    ],
 ];
--- lang/it/translations.php
+++ lang/it/translations.php
@@ -8,4 +8,40 @@ return [
         'create' => 'Crea',
         'edit' => 'Modifica',
     ],
+    'label' => 'Translations',
+    'plural_label' => 'Translations (Plurale)',
+    'navigation' => [
+        'name' => 'Translations',
+        'plural' => 'Translations',
+        'group' => [
+            'name' => 'General',
+            'description' => 'General Settings',
+        ],
+        'label' => 'Translations',
+        'sort' => 1,
+        'icon' => 'heroicon-o-collection',
+    ],
+    'fields' => [
+        'id' => [
+            'label' => 'Identificativo',
+            'tooltip' => 'Identificativo univoco del record',
+        ],
+        'created_at' => [
+            'label' => 'Data Creazione',
+        ],
+        'updated_at' => [
+            'label' => 'Ultima Modifica',
+        ],
+    ],
+    'actions' => [
+        'create' => [
+            'label' => 'Crea Translations',
+        ],
+        'edit' => [
+            'label' => 'Modifica Translations',
+        ],
+        'delete' => [
+            'label' => 'Elimina Translations',
+        ],
+    ],
 ];
