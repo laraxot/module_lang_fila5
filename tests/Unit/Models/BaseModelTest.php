15325b9d428161513e11a4f1e6e7545a8e362df3
marco76tv <marco.sottana@gmail.com>
2026-03-07 17:56:12 +0100

    .

--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace Modules\Lang\Tests\Unit\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Modules\Lang\Models\BaseModel;
 use Tests\TestCase;
 
-uses(TestCase::class, DatabaseTransactions::class);
+uses(TestCase::class);
 
 beforeEach(function () {
     $this->baseModel = new class extends BaseModel {
