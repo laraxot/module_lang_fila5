e5a8fd77c949fd981f8a1a968f224357c9f22647
marco76tv <marco.sottana@gmail.com>
2026-03-12 16:39:48 +0100

    .

--- tests/Unit/Models/BaseModelLangTest.php
+++ tests/Unit/Models/BaseModelLangTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Lang\Tests\Unit\Models;
 
-uses(Modules\Lang\Tests\TestCase::class);
+uses(\Modules\Lang\Tests\TestCase::class);
 
 use Modules\Lang\Models\BaseModelLang;
 
