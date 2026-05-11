0e29e63897a9271b46c821b32aa4de79269ece03
marco76tv <marco.sottana@gmail.com>
2026-05-11 17:10:15 +0200

    .

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -228,7 +228,7 @@ class Post extends BaseModel
      */
     public function getTitleAttribute(?string $value): ?string
     {
-        if (null !== $value) {
+        if ($value !== null) {
             return $value;
         }
 
@@ -250,7 +250,7 @@ class Post extends BaseModel
 
         $this->title = $value;
 
-        if (null !== $this->getKey()) {
+        if ($this->getKey() !== null) {
             $this->update([
                 'title' => $value,
             ]);
@@ -264,11 +264,11 @@ class Post extends BaseModel
      */
     public function getGuidAttribute(?string $value): ?string
     {
-        if (\is_string($value) && '' !== $value && ! str_contains($value, ' ')) {
+        if (\is_string($value) && $value !== '' && ! str_contains($value, ' ')) {
             return $value;
         }
         $value = $this->title;
-        if ('' === $value) {
+        if ($value === '') {
             // Assicuriamoci che i valori siano stringhe prima della concatenazione
             $postType = isset($this->attributes['post_type']) && is_string($this->attributes['post_type'])
                 ? $this->attributes['post_type']
@@ -278,13 +278,13 @@ class Post extends BaseModel
                 : '';
             $value = $postType.' '.$postId;
         }
-        if (null === $value) {
+        if ($value === null) {
             $value = 'u-'.random_int(1, 1000);
         }
         $value = Str::slug($value);
         $this->guid = $value;
 
-        if (null !== $this->getKey()) {
+        if ($this->getKey() !== null) {
             $this->update([
                 'guid' => $value,
             ]);
