2d0ea6245487c58ef91cbb9a1c750f30be03b5cf
marco76tv <marco.sottana@gmail.com>
2026-05-11 17:10:15 +0200

    .

--- app/Filament/Widgets/LanguageSwitcherWidget.php
+++ app/Filament/Widgets/LanguageSwitcherWidget.php
@@ -104,7 +104,7 @@ class LanguageSwitcherWidget extends XotBaseWidget
         // Aggiunge la lingua all'URL
         $path = request()->getPathInfo();
 
-        return url($locale.('/' === $path ? '' : $path));
+        return url($locale.($path === '/' ? '' : $path));
     }
 
     /**
