b68f4f246a2430bceb1471f1ad6f9328f4b3b8c4
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:10:58 +0200

    .

--- app/Filament/Resources/TranslationFileResource/Schemas/TranslationFileForm.php
+++ app/Filament/Resources/TranslationFileResource/Schemas/TranslationFileForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Lang\app\Filament\Resources\TranslationFileResource\Schemas;
+namespace Modules\Lang\app\Filament\Resources\TranslationFileResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
