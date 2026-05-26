9ffcf8d708ae0dfda69ccd1f1ebf02b16e3c2d53
Malebestia <malebestia@yahoo.it>
2026-05-26 11:54:00 +0200

    .

--- app/Filament/Resources/Pages/LangBaseViewRecord.php
+++ app/Filament/Resources/Pages/LangBaseViewRecord.php
@@ -4,33 +4,20 @@ declare(strict_types=1);
 
 namespace Modules\Lang\Filament\Resources\Pages;
 
-<<<<<<< HEAD
 use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
 use LaraZeus\SpatieTranslatable\Resources\Pages\ViewRecord\Concerns\Translatable;
-=======
-// use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
-// use LaraZeus\SpatieTranslatable\Resources\Pages\ViewRecord\Concerns\Translatable;
->>>>>>> e246ed3 (Check & fix styling)
 use Modules\Xot\Filament\Resources\Pages\XotBaseViewRecord;
 
 abstract class LangBaseViewRecord extends XotBaseViewRecord
 {
-<<<<<<< HEAD
     use Translatable;
-=======
-    // use Translatable; // Temporarily disabled until lara-zeus package is working
->>>>>>> e246ed3 (Check & fix styling)
 
     public static string $resource; // = SectionResource::class;
 
     protected function getHeaderActions(): array
     {
         return array_merge(
-<<<<<<< HEAD
             ['locale-switcher' => LocaleSwitcher::make()],
-=======
-            // ['locale-switcher' => LocaleSwitcher::make()], // Temporarily disabled until lara-zeus package is working
->>>>>>> e246ed3 (Check & fix styling)
             parent::getHeaderActions(),
         );
     }
