994848b8ac99ab4ec7de1e9cb0e5058888f08b74
marco76tv <marco.sottana@gmail.com>
2026-05-26 13:33:24 +0200

    .

--- app/Filament/Resources/Pages/LangBaseViewRecord.php
+++ app/Filament/Resources/Pages/LangBaseViewRecord.php
@@ -12,7 +12,7 @@ abstract class LangBaseViewRecord extends XotBaseViewRecord
 {
     use Translatable;
 
-    public static string $resource; // = SectionResource::class;
+    protected static string $resource; // = SectionResource::class;
 
     protected function getHeaderActions(): array
     {
