0b14b0986b6ec96578607b820d38ca8f32e273b7
Malebestia <malebestia@yahoo.it>
2026-05-26 11:57:26 +0200

    .

--- app/Actions/WriteTranslationFileAction.php
+++ app/Actions/WriteTranslationFileAction.php
@@ -94,6 +94,7 @@ class WriteTranslationFileAction
         unlink($tempFile);
 
         if (0 !== $returnCode) {
+<<<<<<< HEAD
             $lines = [];
             foreach ($output as $line) {
                 if (is_string($line)) {
@@ -101,6 +102,9 @@ class WriteTranslationFileAction
                 }
             }
             $error = implode("\n", $lines);
+=======
+            $error = implode("\n", $output ?? []);
+>>>>>>> 0ca50a9 (Check & fix styling)
             throw new \Exception("Sintassi PHP non valida: {$error}");
         }
     }
