1a2746ea9eadd34c97f461cb76e69fd217f40f23
marco76tv <marco76tv@users.noreply.github.com>
2026-06-10 05:41:50 +0000

    Check & fix styling

--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace Modules\Lang\Tests\Unit\Models;
 
+use Illuminate\Database\Eloquent\Model;
+use Modules\Lang\Models\BaseModel;
+use Modules\Lang\Tests\TestCase;
+use PHPUnit\Framework\Assert;
+
+uses(TestCase::class);
+
+function makeLangBaseModel(): BaseModel
+{
+    return new class extends BaseModel {
         protected $table = 'test_lang_table';
     };
 }
