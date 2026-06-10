5a38b7025334f2617bb24a443ca17151d0a8a61f
marco76tv <marco76tv@users.noreply.github.com>
2026-06-10 05:41:50 +0000

    Check & fix styling

--- app/Services/TranslatorService.php
+++ app/Services/TranslatorService.php
@@ -20,7 +20,8 @@ class TranslatorService extends LaravelTranslator
     /**
      * Get the translation for the given key.
      *
-     * @param  array<string, mixed>  $replace
+     * @param array<string, mixed> $replace
+     *
      * @return string|array<string, mixed>
      */
     public function get(mixed $key, array $replace = [], mixed $locale = null, mixed $fallback = true): string|array
