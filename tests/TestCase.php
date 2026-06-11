d3849db26b3849e6f3e8c0ed6cdcc8d9ffe1a869
Malebestia <Malebestia@users.noreply.github.com>
2026-06-11 07:50:56 +0000

    Check & fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -61,7 +61,7 @@ abstract class TestCase extends XotBaseTestCase
     }
 
     /**
-     * @param  array<string, mixed>  $data
+     * @param array<string, mixed> $data
      */
     public function assertDatabaseHasRow(string $table, array $data): void
     {
@@ -69,12 +69,12 @@ abstract class TestCase extends XotBaseTestCase
     }
 
     /**
-     * @param  class-string<\Throwable>  $exceptionClass
+     * @param class-string<\Throwable> $exceptionClass
      */
     public function expectApplicationException(string $exceptionClass, ?string $message = null): void
     {
         $this->expectException($exceptionClass);
-        if ($message !== null) {
+        if (null !== $message) {
             $this->expectExceptionMessage($message);
         }
     }
