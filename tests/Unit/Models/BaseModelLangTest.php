2a158a473462f227b02a483de13c06d1f9c4ab2c
marco76tv <marco76tv@users.noreply.github.com>
2026-06-15 06:48:35 +0000

    Check & fix styling

--- tests/Unit/Models/BaseModelLangTest.php
+++ tests/Unit/Models/BaseModelLangTest.php
@@ -8,7 +8,7 @@ use Modules\Lang\Models\BaseModelLang;
 use Modules\Lang\Tests\TestCase;
 use PHPUnit\Framework\Assert;
 
-uses(\Modules\Lang\Tests\TestCase::class);
+uses(TestCase::class);
 
 describe('BaseModelLang', function () {
     test('has correct connection', function () {
