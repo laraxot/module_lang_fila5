ba13a3efd72c77de582c3ab13112d1711cdb1c3d
marco76tv <marco76tv@users.noreply.github.com>
2026-07-06 07:36:48 +0000

    Check & fix styling

--- tests/Unit/Actions/ReadTranslationFileActionTest.php
+++ tests/Unit/Actions/ReadTranslationFileActionTest.php
@@ -41,7 +41,7 @@ function readTranslationTestFilePath(): string
 
 function makeReadTranslationFileAction(): ReadTranslationFileAction
 {
-    return new ReadTranslationFileAction;
+    return new ReadTranslationFileAction();
 }
 
 afterEach(function (): void {
