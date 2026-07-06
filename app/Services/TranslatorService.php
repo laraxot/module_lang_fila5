3042d25a36e89aa7f965f795131e0ac417854efb
marco76tv <marco.sottana@gmail.com>
2026-07-06 09:36:25 +0200

    .

--- app/Services/TranslatorService.php
+++ app/Services/TranslatorService.php
@@ -11,14 +11,20 @@ namespace Modules\Lang\Services;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Translation\Translator as LaravelTranslator;
 use Modules\Lang\Models\Translation;
+use Spatie\QueueableAction\QueueableAction;
 
 class TranslatorService extends LaravelTranslator
 {
+    use QueueableAction;
+
     /** @var Dispatcher */
     protected $events;
 
     /**
      * Get the translation for the given key.
+     *
+     * @param  array<string, mixed>  $replace
+     * @return string|array<string, mixed>
      */
     public function get(mixed $key, array $replace = [], mixed $locale = null, mixed $fallback = true): string|array
     {
@@ -29,6 +35,13 @@ class TranslatorService extends LaravelTranslator
 
             // Reget with fallback
             $result = parent::get($key, $replace, $locale, $fallback);
+        }
+
+        if (is_array($result)) {
+            /** @var array<string, mixed> $arrayResult */
+            $arrayResult = $result;
+
+            return $arrayResult;
         }
 
         return $result;
@@ -55,4 +68,6 @@ class TranslatorService extends LaravelTranslator
         ];
         Translation::firstOrCreate($data);
     }
+
+    public function execute(): void {}
 }
