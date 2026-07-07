07c6c204857ee41c545b4f5d753622558935569a
Malebestia <Malebestia@users.noreply.github.com>
2026-07-07 08:48:59 +0000

    Check & fix styling

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,11 +17,11 @@ class EventServiceProvider extends BaseEventServiceProvider
 
     /**
      * Indicates if events should be discovered.
-<<<<<<< HEAD
-=======
+     * <<<<<<< HEAD
+     * =======.
      *
      * @var bool
->>>>>>> origin/dev
+     *           >>>>>>> origin/dev
      */
     protected static $shouldDiscoverEvents = true;
 
