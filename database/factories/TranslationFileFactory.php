c97ab062f22a75533a5ce74795106764f9806968
marco76tv <marco76tv@users.noreply.github.com>
2026-07-08 07:03:45 +0000

    Check & fix styling

--- database/factories/TranslationFileFactory.php
+++ database/factories/TranslationFileFactory.php
@@ -21,6 +21,7 @@ class TranslationFileFactory extends Factory
 
     /**
      * Define the model's default state.
+     *
      * @return array<string, mixed>
      */
     public function definition(): array
