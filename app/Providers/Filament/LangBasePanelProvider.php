f131056ecf9fa1fc1518f0bfcd64b094a1e57d57
marco76tv <marco.sottana@gmail.com>
2026-07-08 08:48:45 +0200

    .

--- app/Providers/Filament/LangBasePanelProvider.php
+++ app/Providers/Filament/LangBasePanelProvider.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 namespace Modules\Lang\Providers\Filament;
 
 use Filament\Panel;
-<<<<<<< HEAD
 use LaraZeus\SpatieTranslatable\SpatieTranslatablePlugin;
-=======
-// use LaraZeus\SpatieTranslatable\SpatieTranslatablePlugin;
->>>>>>> e246ed3 (Check & fix styling)
 use Modules\Xot\Providers\Filament\XotBasePanelProvider;
 
 abstract class LangBasePanelProvider extends XotBasePanelProvider
@@ -28,17 +24,10 @@ abstract class LangBasePanelProvider extends XotBasePanelProvider
         //     'filament-navigation'
         // );
 
-<<<<<<< HEAD
         $spatieLaravelTranslatablePlugin = SpatieTranslatablePlugin::make()->defaultLocales(['en', 'it']);
         $panel->plugins([
             $spatieLaravelTranslatablePlugin,
         ]);
-=======
-        // $spatieLaravelTranslatablePlugin = SpatieTranslatablePlugin::make()->defaultLocales(['en', 'it']);
-        // $panel->plugins([
-        //     $spatieLaravelTranslatablePlugin,
-        // ]);
->>>>>>> e246ed3 (Check & fix styling)
 
         return $panel;
     }
