d3f0d95790834a05c5e1d0b2c3524b2758049a1e
marco76tv <marco76tv@users.noreply.github.com>
2026-07-08 06:51:53 +0000

    Check & fix styling

--- app/Http/Livewire/Lang/Switcher.php
+++ app/Http/Livewire/Lang/Switcher.php
@@ -35,7 +35,7 @@ class Switcher extends Component
         $this->url = Request::getRequestUri();
         $langs = Arr::map($langs, function (array $item, string $key) {
             $url = LaravelLocalization::getLocalizedURL($key, $this->url, [], true);
-            if ($url !== false) {
+            if (false !== $url) {
                 // Verifichiamo che $url sia una stringa o lo convertiamo in modo sicuro
                 if (! is_string($url)) {
                     // Se non è una stringa, utilizziamo una URL di fallback
