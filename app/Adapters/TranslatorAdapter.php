b506ec649dc28498d3bc19f90c8ba4ada1e0c0e1
marco76tv <marco76tv@users.noreply.github.com>
2026-07-14 21:49:33 +0000

    Check & fix styling

--- app/Adapters/TranslatorAdapter.php
+++ app/Adapters/TranslatorAdapter.php
@@ -43,7 +43,7 @@ class TranslatorAdapter extends LaravelTranslator
         }
 
         if (is_array($result)) {
-            /** @var array<string, mixed> $result */
+            /* @var array<string, mixed> $result */
             return $result;
         }
 
