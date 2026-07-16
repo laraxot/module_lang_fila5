dad69f4067c954a06248ed5318f50b8319a0edf5
marco76tv <marco.sottana@gmail.com>
2026-07-16 09:46:41 +0200

    .

--- app/Adapters/TranslatorAdapter.php
+++ app/Adapters/TranslatorAdapter.php
@@ -27,8 +27,7 @@ class TranslatorAdapter extends LaravelTranslator
     /**
      * Get the translation for the given key.
      *
-     * @param array<string, mixed> $replace
-     *
+     * @param  array<string, mixed>  $replace
      * @return string|array<array-key, mixed>
      */
     public function get(mixed $key, array $replace = [], mixed $locale = null, mixed $fallback = true): string|array
