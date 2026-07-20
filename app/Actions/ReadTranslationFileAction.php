656e5970ba782b2c39a4d1aeebb6b44f68132a01
marco76tv <marco76tv@users.noreply.github.com>
2026-07-20 17:31:14 +0000

    Check & fix styling

--- app/Actions/ReadTranslationFileAction.php
+++ app/Actions/ReadTranslationFileAction.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Lang\Actions;
 
-use Exception;
 use Spatie\QueueableAction\QueueableAction;
 use Webmozart\Assert\Assert;
 
@@ -24,18 +23,18 @@ class ReadTranslationFileAction
     public function execute(string $filePath): array
     {
         if (! file_exists($filePath)) {
-            throw new Exception("File di traduzione non trovato: {$filePath}");
+            throw new \Exception("File di traduzione non trovato: {$filePath}");
         }
 
         if (! is_readable($filePath)) {
-            throw new Exception("File di traduzione non leggibile: {$filePath}");
+            throw new \Exception("File di traduzione non leggibile: {$filePath}");
         }
 
         // Carica il file di traduzione
         $translations = require $filePath;
 
         if (! is_array($translations)) {
-            throw new Exception("File di traduzione non valido: {$filePath}");
+            throw new \Exception("File di traduzione non valido: {$filePath}");
         }
 
         Assert::isArray($translations);
