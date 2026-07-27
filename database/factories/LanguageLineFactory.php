25c77fc9baa1f2a2e7a264260161c9c314ad0919
marco76tv <marco.sottana@gmail.com>
2026-07-27 12:29:31 +0200

    .

--- database/factories/LanguageLineFactory.php
+++ database/factories/LanguageLineFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Lang\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+<<<<<<< HEAD
 
 /**
  * @extends Factory<\Modules\Lang\Models\LanguageLine>
@@ -22,5 +23,31 @@ class LanguageLineFactory extends Factory
     public function definition(): array
     {
         return [];
+=======
+use Modules\Lang\Models\LanguageLine;
+
+/**
+ * @extends Factory<LanguageLine>
+ */
+class LanguageLineFactory extends Factory
+{
+    /** @var class-string<LanguageLine> */
+    protected $model = LanguageLine::class;
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'group' => $this->faker->randomElement(['validation', 'auth', 'pagination', 'passwords']),
+            'key' => $this->faker->unique()->slug(2),
+            'text' => [
+                'it' => $this->faker->sentence(),
+                'en' => $this->faker->sentence(),
+            ],
+            'locale' => $this->faker->randomElement(['it', 'en']),
+        ];
+>>>>>>> 677f1f5 (.)
     }
 }
