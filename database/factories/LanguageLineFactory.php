7205be4c1503966b99b4fa6cc72876881bdc54e4
marco76tv <marco.sottana@gmail.com>
2026-07-31 11:12:53 +0200

    .

--- database/factories/LanguageLineFactory.php
+++ database/factories/LanguageLineFactory.php
@@ -5,25 +5,6 @@ declare(strict_types=1);
 namespace Modules\Lang\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-<<<<<<< HEAD
-
-/**
- * @extends Factory<\Modules\Lang\Models\LanguageLine>
- */
-class LanguageLineFactory extends Factory
-{
-    /**
-     * The name of the factory's corresponding model.
-     */
-    protected $model = \Modules\Lang\Models\LanguageLine::class;
-
-    /**
-     * Define the model's default state.
-     */
-    public function definition(): array
-    {
-        return [];
-=======
 use Modules\Lang\Models\LanguageLine;
 
 /**
@@ -48,6 +29,5 @@ class LanguageLineFactory extends Factory
             ],
             'locale' => $this->faker->randomElement(['it', 'en']),
         ];
->>>>>>> 677f1f5 (.)
     }
 }
