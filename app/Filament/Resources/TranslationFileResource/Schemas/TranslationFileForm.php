6d6b8c31374cbc0a5d7908ce01cbcaaa56f4f882
Marco Xot <marco76tv@hotmail.com>
2026-08-05 23:03:53 +0200

    .

--- app/Filament/Resources/TranslationFileResource/Schemas/TranslationFileForm.php
+++ app/Filament/Resources/TranslationFileResource/Schemas/TranslationFileForm.php
@@ -14,11 +14,7 @@ class TranslationFileForm extends XotBaseResourceForm
     /**
      * @return array<string, Component>
      */
-<<<<<<< HEAD
     public static function getFormSchema(): array
-=======
-    public static function getFormSchemaOld(): array
->>>>>>> laraxot/dev
     {
         return [
             'name' => Section::make([
