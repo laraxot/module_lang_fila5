1e421a9ab6adea0494b404896cbb9b2fcf4c3dfa
Marco Xot <marco76tv@hotmail.com>
2026-08-31 13:06:06 +0200

    .

--- tests/Unit/Actions/TransArrayActionTest.php
+++ tests/Unit/Actions/TransArrayActionTest.php
@@ -8,7 +8,7 @@ use Modules\Lang\Actions\TransArrayAction;
 use Modules\Lang\Tests\TestCase;
 use PHPUnit\Framework\Assert;
 
-uses(TestCase::class);
+uses(\Modules\Lang\Tests\TestCase::class);
 
 function makeTransArrayAction(): TransArrayAction
 {
