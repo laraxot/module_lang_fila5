b254d3ca4fc63f74387400034784fd18123ed82f
Marco Xot <marco76tv@hotmail.com>
2026-09-08 16:54:40 +0200

    .

--- tests/Fixtures/LangBaseResourceStub.php
+++ tests/Fixtures/LangBaseResourceStub.php
@@ -10,12 +10,4 @@ use Modules\Lang\Models\TranslationFile;
 final class LangBaseResourceStub extends LangBaseResource
 {
     protected static ?string $model = TranslationFile::class;
-
-    /**
-     * @return array<string, mixed>
-     */
-    public static function getFormSchema(): array
-    {
-        return [];
-    }
 }
