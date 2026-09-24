e1b21dfe1b55eb8f5653691f0ceadbd52f92d664
marco76tv <marco76tv@users.noreply.github.com>
2026-09-24 10:41:49 +0000

    Check & fix styling

--- app/Models/LanguageLine.php
+++ app/Models/LanguageLine.php
@@ -11,13 +11,14 @@ use Modules\Xot\Contracts\ProfileContract;
 /**
  * Modules\Lang\Models\LanguageLine.
  *
-<<<<<<< .merge_file_A5l4SK
- * @property int $id
- * @property string $group
- * @property string $key
+ * <<<<<<< .merge_file_A5l4SK
+ *
+ * @property int                     $id
+ * @property string                  $group
+ * @property string                  $key
  * @property array<array-key, mixed> $text
-=======
-<<<<<<< .merge_file_rXdZB3
+ *                                               =======
+ *                                               <<<<<<< .merge_file_rXdZB3
  * @property int                     $id
  * @property string                  $group
  * @property string                  $key
@@ -27,25 +28,25 @@ use Modules\Xot\Contracts\ProfileContract;
  * @property string|null             $updated_by
  * @property Carbon|null             $created_at
  * @property Carbon|null             $updated_at
-=======
- * @property int $id
- * @property string $group
- * @property string $key
-<<<<<<< .merge_file_Wf48e2
+ *                                               =======
+ * @property int                     $id
+ * @property string                  $group
+ * @property string                  $key
+ *                                               <<<<<<< .merge_file_Wf48e2
  * @property array<array-key, mixed> $text
-=======
- * @property array<string, string> $text
->>>>>>> .merge_file_ThtvFr
->>>>>>> .merge_file_6T9MPX
- * @property string $locale
- * @property string|null $created_by
- * @property string|null $updated_by
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
-<<<<<<< .merge_file_A5l4SK
-=======
->>>>>>> .merge_file_OJ1APb
->>>>>>> .merge_file_6T9MPX
+ *                                               =======
+ * @property array<string, string>   $text
+ *                                               >>>>>>> .merge_file_ThtvFr
+ *                                               >>>>>>> .merge_file_6T9MPX
+ * @property string                  $locale
+ * @property string|null             $created_by
+ * @property string|null             $updated_by
+ * @property Carbon|null             $created_at
+ * @property Carbon|null             $updated_at
+ *                                               <<<<<<< .merge_file_A5l4SK
+ *                                               =======
+ *                                               >>>>>>> .merge_file_OJ1APb
+ *                                               >>>>>>> .merge_file_6T9MPX
  *
  * @method static EloquentBuilder<static>|LanguageLine newModelQuery()
  * @method static EloquentBuilder<static>|LanguageLine newQuery()
@@ -60,21 +61,22 @@ use Modules\Xot\Contracts\ProfileContract;
  * @method static EloquentBuilder<static>|LanguageLine whereCreatedBy($value)
  * @method static EloquentBuilder<static>|LanguageLine whereUpdatedBy($value)
  *
-<<<<<<< .merge_file_A5l4SK
- * @property-read ProfileContract|null $creator
- * @property-read ProfileContract|null $deleter
- * @property-read ProfileContract|null $updater
-=======
-<<<<<<< .merge_file_rXdZB3
+ * <<<<<<< .merge_file_A5l4SK
+ *
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $deleter
  * @property ProfileContract|null $updater
-=======
- * @property-read ProfileContract|null $creator
- * @property-read ProfileContract|null $deleter
- * @property-read ProfileContract|null $updater
->>>>>>> .merge_file_OJ1APb
->>>>>>> .merge_file_6T9MPX
+ *                                         =======
+ *                                         <<<<<<< .merge_file_rXdZB3
+ * @property ProfileContract|null $creator
+ * @property ProfileContract|null $deleter
+ * @property ProfileContract|null $updater
+ *                                         =======
+ * @property ProfileContract|null $creator
+ * @property ProfileContract|null $deleter
+ * @property ProfileContract|null $updater
+ *                                         >>>>>>> .merge_file_OJ1APb
+ *                                         >>>>>>> .merge_file_6T9MPX
  *
  * @mixin \Eloquent
  */
