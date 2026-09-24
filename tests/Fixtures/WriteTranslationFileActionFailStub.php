87ca3ebae411300ef724d9e3cae61bf1b2afcb82
Marco Xot <marco.sottana@gmail.com>
2026-09-24 06:44:34 +0000

    .

---
+++ tests/Fixtures/WriteTranslationFileActionFailStub.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Lang\Tests\Fixtures;
+
+use Modules\Lang\Actions\WriteTranslationFileAction;
+
+final class WriteTranslationFileActionFailStub extends WriteTranslationFileAction
+{
+    protected function putTranslationFile(string $filePath, string $phpContent): false
+    {
+        return false;
+    }
+}
