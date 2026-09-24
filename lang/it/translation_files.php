0216a0ea4ed97a00f7c7c9063804feb0e16a138f
Marco Xot <marco.sottana@gmail.com>
2026-09-24 06:44:27 +0000

    .

---
+++ lang/it/translation_files.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'id' => ['label' => 'id'],
+        'name' => ['label' => 'name'],
+        'created_at' => ['label' => 'created_at'],
+        'key' => ['label' => 'key'],
+        'path' => ['label' => 'path'],
+    ],
+];
